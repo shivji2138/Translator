e50f19556c3014185c3177441cf4b7f5d7c99acb
shivji2138 <157487474+shivji2138@users.noreply.github.com>
2024-05-26 19:40:00 +0530

    Update and rename queries.txt to queries.sql

---
+++ queries.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE translator;
+USE translator;
+create table translations(
+s_no int primary key auto_increment,
+english varchar(100),
+tamil varchar(100),
+hindi varchar(100),
+Bengali varchar(100),
+Telugu varchar(100),
+Marathi varchar(100),
+Gujarati varchar(100),
+Kannada varchar(100),
+Malayalam varchar(100),
+Punjabi varchar(100)
+);
